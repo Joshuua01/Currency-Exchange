72aded923938146ba1cd170ac7e50badaaef2847
Jan Steinbichler <Jan.Steinbichler@microsoft.wsei.edu.pl>
2022-01-29 22:45:49 +0100

    Styling and swap button

--- script.js
+++ script.js
@@ -12,15 +12,36 @@ function getExchange(from, to) {
             });
     }
 
-    window.onload = () => {
-        sendRequest("codes", (data) => {
-            data.supported_codes.forEach(element => {
-                $("#currency-from").append("<option>" + element[0] + "</option>");
-                $("#currency-to").append("<option>" + element[0] + "</option>");
-            });
-        })
-        $(".exchange-btn").click((e) => {
-            if ($("#amount").val() == '') return;
-            getExchange($("#currency-from").val(), $("#currency-to").val())
-        })
-    }
+window.onload = () => {
+    $('.swap-button').click(() => {
+        let from = $("#currency-from").val();
+        let to = $("#currency-to").val();
+        $("#currency-from").val(to);
+        $("#currency-to").val(from);
+    })
+
+    $('.amount').focus(() => {
+        $('.amount-label').addClass('focused');
+    });
+
+    $('.amount').focusout(() => {
+        if($('.amount').val() == ''){
+        $('.amount-label').removeClass('focused');
+        }
+    });
+    $('.amount').keypress(function (e) { 
+        if(e.key == 'e'){
+            e.preventDefault();
+        }
+    });
+            sendRequest("codes", (data) => {
+                data.supported_codes.forEach(element => {
+                    $("#currency-from").append("<option>" + element[0] + "</option>");
+                    $("#currency-to").append("<option>" + element[0] + "</option>");
+                });
+            })
+            $(".exchange-btn").click((e) => {
+                if ($("#amount").val() == '') return;
+                getExchange($("#currency-from").val(), $("#currency-to").val())
+            })
+}
